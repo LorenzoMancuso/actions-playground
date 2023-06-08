3068aefdbee280c969abb4333287927d69dbcdbe
Lorenzo Mancuso <mancusolorenzo95@gmail.com>
2023-06-08 21:39:49 +0200

    Add github workflows for app-runner and ecs

--- app/app.py
+++ app/app.py
@@ -9,7 +9,9 @@ def hello():
 
 
 """
-Because we had the "if" statement in our application file, this will be true if we run this module as a standalone program. As a result, it can function as a module imported by another program or as a standalone program, but it will only execute the code in the if statement if run as a program.
+Because we had the "if" statement in our application file, this will be true if we run this module as a standalone program. 
+As a result, it can function as a module imported by another program or as a standalone program, 
+but it will only execute the code in the if statement if run as a program.
 """
 if __name__ == "__main__":
     app.run(debug=True)
